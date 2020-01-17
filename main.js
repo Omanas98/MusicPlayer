206db6e580968c8e7b3dcea322ec3c05078436ae
Omar <Omanas_98@hotmail.com>
2020-01-17 14:52:09 +0100

    Nu funkar alla låtar och bil&text byten vid klicka på play knapparna

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-$('#people').on('click', 'button', showProfile)
-
-play
-
-function showProfile(){
-     let image = $(this).parent().find('#myImage').attr('src')
-     $('#allSongs img').attr('src', image)
+let audio = document.getElementById("audio")
+function play(){
+    audio.play();
 }
 
+function pause(){
+    audio.pause();
+}
 
+function marked(){
+    let active = on('click','.playlist', play());
+    $('playlist').click();
+}
 
+$( "li" ).eq( 2 ).css( "background-color", "#FF8066" );
 
+$( "li" ).eq( -2 ).css( "background-color", "#FF8066" );
 
+$( "li" ).eq( 5 ).css( "background-color", "#FF8066" );
 
+$( "body" ).find( "div" ).eq( 2 ).addClass( "grey");
 
+$(this).find(".playlist");
 
+$('.playlist').on('click', '.fa-play-circle', showSong)
 
+function showSong(){
+    let image = $(this).closest('.playlist').find('img').attr('src')
+    $('.musicPlayer img').attr('src', image)
+
+    let name = $(this).closest('.playlist').find('h4').text()
+    $('.musicPlayer h4').text(name)
+
+    let songs = $(this).closest('.playlist').find('audio').attr('src')
+
+    $('.musicPlayer audio').attr('src', songs)
+    play()
+}
